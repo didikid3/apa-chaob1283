95dc119e7a674d46c9b7dfdbd1608ff52c4971b6
didikid3 <34197234+didikid3@users.noreply.github.com>
2019-02-28 22:36:03 -0800

    Update NumberSorter.java

--- U9A-sortsearch/NumberSorter.java
+++ U9A-sortsearch/NumberSorter.java
@@ -8,17 +8,27 @@ import java.io.File;
 import java.io.IOException;
 import static java.lang.System.*;
 
-public class NumberSorter
+class NumberSorter
 {
-
-	public static void  getSortedDigitArray(int number)
+	public static void  getSortedDigitArray(int orig)
 	{
-		int[] sorted = null;
+		int number = orig;
+    		int[] sorted = null;
 		int count = 0;
-		while(number >= 10){
+		while(number > 0){
 			number = number/10;
 			count++;
 		}			
-		return sorted;
+		sorted = new int[count];
+    		number = orig;
+    		for(int i=0;i<sorted.length;i++){
+        		sorted[i]=(number%10);
+        		number = number/10;
+   		}
+
+    		Arrays.sort(sorted);
+    		for(int item:sorted){
+      			System.out.print(item + " ");
+    		}
 	}
 }
