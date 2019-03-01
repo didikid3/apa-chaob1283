2c897bac995de7bd761f57ed3291a2f5424d605e
didikid3 <34197234+didikid3@users.noreply.github.com>
2019-02-28 23:10:32 -0800

    Update WordSortTwo.java

--- U9A-sortsearch/WordSortTwo.java
+++ U9A-sortsearch/WordSortTwo.java
@@ -8,19 +8,35 @@ import static java.lang.System.*;
 
 public class WordSortTwo
 {
-	private String[] wordRay;
-
-	public WordSortTwo(String sentence)
+	public static void WordSortTwo(String sentence)
 	{
-	}
-
-	public void sort()
-	{
-	}
-
-	public String toString()
-	{
-		String output="";
-		return output+"\n\n";
-	}
+    		int count = 0;
+    		char word;
+    		for(int i=0;i<sentence.length();i++){
+      			word = sentence.charAt(i);
+      			if(word == ' ')
+        			count++;
+    		}
+    		String[] lets = new String[count+1];
+    		String add="";
+    		int index = 0;
+    		for(int m=0;m<sentence.length();m++){
+      			word = sentence.charAt(m);
+        		if(word == ' '){
+          			lets[index] = add;
+          			add = "";
+          			index ++;
+        		}
+        		else{
+			  	add = add + sentence.charAt(m);
+          			if(m == sentence.length()-1){
+            				lets[index] = add;
+          			}
+        		}
+    		}
+    		Arrays.sort(lets);
+    		for(String item:lets){
+      			System.out.println(item);
+    		}
+  	}
 }
