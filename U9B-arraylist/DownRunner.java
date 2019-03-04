9e24acfdd6f815237f66a0f379329c6d6acd362a
didikid3 <34197234+didikid3@users.noreply.github.com>
2019-03-03 20:43:01 -0800

    Update DownRunner.java

--- U9B-arraylist/DownRunner.java
+++ U9B-arraylist/DownRunner.java
@@ -6,6 +6,12 @@
 public class DownRunner
 {
   public static void main( String args[] )
-  {											
+  {	
+    ArrayList<Integer> array = new ArrayList<Integer>();
+    array.add(500);
+    array.add(3);
+    array.add(40);
+    array.add(2);
+    System.out.println(ListDown.go(array));
   }
 }
