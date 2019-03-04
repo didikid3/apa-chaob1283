534d14f5263eef2abae8001ef1a024d32467ee21
didikid3 <34197234+didikid3@users.noreply.github.com>
2019-03-03 20:24:09 -0800

    Update ListSumFirst.java

--- U9B-arraylist/ListSumFirst.java
+++ U9B-arraylist/ListSumFirst.java
@@ -10,6 +10,15 @@ public class ListSumFirst
 {
   public static int go(List<Integer> ray)
   {
-    return 0;
+    int first = ray.get(0);
+    int sum = 0;
+    if(ray.size()<=1)
+      return -1;
+    for(int i = 1; i<ray.size();i++){
+      if(ray.get(i)>first){
+        sum+=ray.get(i);
+      }
+    }
+    return sum;
   }
 }
