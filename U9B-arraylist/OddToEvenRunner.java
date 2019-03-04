b1415d155259e894d24ffd350341185d9f5c9e63
didikid3 <34197234+didikid3@users.noreply.github.com>
2019-03-03 20:36:50 -0800

    Update OddToEvenRunner.java

--- U9B-arraylist/OddToEvenRunner.java
+++ U9B-arraylist/OddToEvenRunner.java
@@ -7,5 +7,12 @@ public class OddToEvenRunner
 {
   public static void main( String args[] )
   {
+    ArrayList<Integer> array = new ArrayList<Integer>();
+    array.add(300);
+    array.add(1);
+    array.add(52);
+    array.add(43);
+    array.add(26);
+    System.out.println(ListOddToEven.go(array));
   }
 }
