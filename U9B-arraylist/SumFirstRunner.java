ed3f87e362fe4fec1d3ee5af7e5c3a9447213f4f
didikid3 <34197234+didikid3@users.noreply.github.com>
2019-03-03 20:27:30 -0800

    Update SumFirstRunner.java

--- U9B-arraylist/SumFirstRunner.java
+++ U9B-arraylist/SumFirstRunner.java
@@ -7,6 +7,13 @@ public class SumFirstRunner
 {
   public static void main( String args[] )
   {
-											
+	ArrayList<Integer> array = new ArrayList<Integer>();
+	  array.add(1);
+	  array.add(3);
+	  array.add(10);
+	  array.add(0);
+	  array.add(-3);
+	  array.add(100);
+	System.out.println(ListSumFirst.go(array));										
   }
 }
